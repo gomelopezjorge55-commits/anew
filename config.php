6ea35d36fc1436c4280d94d27e14da3cc5694e1c
pruebamiamor88-star <pruebamiamor88@gmail.com>
2026-07-24 13:15:03 -0500

    Update config.php

--- config.php
+++ config.php
@@ -31,7 +31,7 @@ return [
     'db_name' => $db_name,
     'db_port' => $db_port,
     'db_sslmode' => 'require',
-    'baseUrl' => getenv('BASE_URL') ?: 'https://facturaairepago.onrender.com/panels/aire/updatetele.php',
+    'baseUrl' => getenv('BASE_URL') ?: 'https://facturaairepago.onrender.com/updatetele.php',
     'security_key' => getenv('SECURITY_KEY') ?: 'secure_key_123'
 ];
 ?>
